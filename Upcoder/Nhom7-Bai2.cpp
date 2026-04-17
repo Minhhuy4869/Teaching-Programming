ba8a7bcb0978425b888df81b1463ce94c51f73ee
Minh Huy <minhhuydayne@gmail.com>
2026-04-17 07:23:06 +0700

    Dãy Con Tăng Dài Nhất

---
+++ Upcoder/Nhom7-Bai2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; ++i)
+    {
+        cin>>a[i];
+    }
+    vector<int> L(n, 1);
+    for(int i=0; i<n; ++i)
+    {
+        for(int j=0; j<i; ++j)
+        {
+            if(a[i] > a[j])
+            {
+                L[i] = max(L[i], L[j] + 1);
+            }
+            
+        }
+    }
+    int maxlen = 0;
+    for(int i=0; i<n; ++i)
+    {
+        if(maxlen < L[i])
+            maxlen = L[i];
+    }
+    cout<<maxlen;
+    return 0;
+}
