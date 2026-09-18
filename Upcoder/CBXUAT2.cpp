ab71d12f1998045e9aa95263ac02905b7d92f24e
duy2913 <nguyendacduy29012013@gmail.com>
2026-09-18 18:03:52 +0700

    Xuất cơ bản 2

---
+++ Upcoder/CBXUAT2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Co gang hoc lap trinh tot - nhung dao duc phai tot nua" << endl;
+    cout << "\"Vi tuong lai\"\n";
+    cout << "'Tat ca co len'\n";
+    cout << "\n";
+    cout << "Ghi nho cac ky tu va lenh dac biet:" << endl;
+    cout << "\\n - xuong hang" << endl;
+    cout << "endl - xuong hang" << '\n';
+    cout << "\\t - tab";
+
+    return 0;
+}
